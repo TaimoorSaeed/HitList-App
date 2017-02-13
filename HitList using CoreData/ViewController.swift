0100258727c68b98975e3422f96440912435f6f5
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-13 14:10:23 +0500

    structure

---
+++ HitList using CoreData/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HitList using CoreData
+//
+//  Created by Nano Degree on 13/02/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
