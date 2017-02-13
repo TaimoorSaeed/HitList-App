bfc91cc179ba89a86c6521bd578c582a80f2827a
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-13 17:42:56 +0500

    completed

--- HitList using CoreData/ViewController.swift
+++ HitList using CoreData/ViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
 
-    var names = [String]()
+//    var names = [String]()
+    
+    var people = [NSManagedObject]()
+    
     
     //MARK : Outlets and Actions
     
@@ -24,7 +28,10 @@ class ViewController: UIViewController, UITableViewDataSource {
         let saveAction = UIAlertAction(title: "Save", style: .default, handler: {(actions:UIAlertAction) -> Void in
             
             let textFeild = alert.textFields!.first
-            self.names.append(textFeild!.text!)
+//            self.names.append(textFeild!.text!)
+//            self.tableView.reloadData()
+            
+            self.saveName(name: textFeild!.text!)
             self.tableView.reloadData()
         })
         
@@ -50,17 +57,44 @@ class ViewController: UIViewController, UITableViewDataSource {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //2
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
+        
+        //3
+        do {
+            let results =
+                try context.fetch(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
     // MARK : UITableVIewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return names.count
+//        return names.count
+            return people.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        cell!.textLabel!.text = names[indexPath.row]
+//        cell!.textLabel!.text = names[indexPath.row]
+        
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text = person.value(forKey: "name") as? String
         
         return cell!
     }
@@ -70,7 +104,36 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.didReceiveMemoryWarning()
         
     }
-
+    
+    
+    func saveName(name: String) {
+        //1
+//        let appDelegate =
+//            UIApplication.shared.delegate as! AppDelegate
+//        
+//         let managedContext = appDelegate.managedObjectContext
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //2
+        let entity =  NSEntityDescription.entity(forEntityName: "Person",
+                                                 in:context)
+        
+        let person = NSManagedObject(entity: entity!,
+                                     insertInto:context)
+        
+        //3
+        person.setValue(name, forKey: "name")
+        
+        //4
+        do {
+            try context.save()
+            //5
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
 
 }
 
